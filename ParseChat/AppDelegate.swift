f0d64cd9b7ada1d3d26a83cf0ba0a4123f9aa83b
Wieniek Sliwinski <wieniek@gmail.com>
2017-04-13 03:55:15 +0100

    Milestone

--- ParseChat/AppDelegate.swift
+++ ParseChat/AppDelegate.swift
@@ -17,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    
+    Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+      configuration.applicationId = "CodePath-Parse"
+      configuration.server = "http://45.79.67.127:1337/parse"
+      
+    }))
+    
     return true
   }
 
---
+++ ParseChat/LoginViewController.swift
@@ -0,0 +1,84 @@
+//
+//  LoginViewController.swift
+//  ParseChat
+//
+//  Created by Wieniek Sliwinski on 4/13/17.
+//  Copyright © 2017 Home User. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+  
+  @IBOutlet weak var loginField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+  
+  var login: String {
+    get {
+      return loginField.text ?? ""
+    }
+  }
+  
+  var password: String {
+    get {
+      return passwordField.text ?? ""
+    }
+  }
+  
+  
+  
+  @IBAction func signupButton(_ sender: UIButton) {
+    
+    let user = PFUser()
+    user.username = login
+    user.password = password
+    user.email = login
+    
+    user.signUpInBackground { (success: Bool, error: Error?) in
+      if let error = error {
+        let errorString = error.localizedDescription
+      } else {
+        print("Let's use the app")
+      }
+    }
+    
+  }
+  
+  @IBAction func loginButton(_ sender: UIButton) {
+    
+    PFUser.logInWithUsername(inBackground: login, password: password) { (user: PFUser?, error: Error?) in
+      if user != nil {
+        print("Successful login")
+      } else {
+        print("Error")
+      }
+    }
+    
+    
+  }
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
